b72ca5b9ad7ea1c6b0b714ff7107bfee349a6c20
Nikita Fomin <97741120+r1xelraze@users.noreply.github.com>
2022-04-02 13:22:11 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ваше предложение:");
+        String words = scan.nextLine();
+        String[] transwords = words.split("[\\d\\s]+");
+        System.out.println("Кол-во слов: ");
+        System.out.print(transwords.length);
+
+
+    }
+}
